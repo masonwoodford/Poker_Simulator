1152ff393c0ba47f53e3a08704793962de29e0fc
masonwoodford <mwoodfor@ucsc.edu>
2020-12-25 18:59:54 -0800

    working on scoring

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 import numpy as np
 import itertools
 
-
 class Card:
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
-        #self.img = ...
 
     def __str__(self):
         return self.value + " " + self.suit
+
+class Hand:
+    def __init__(self, first, second, third, fourth, fifth):
+        self.cards = [first, second, third, fourth, fifth]
+        self.values = [first.value, second.value, third.value, fourth.value, fifth.value]
+        self.suit = [first.suit, second.suit, third.suit, fourth.suit, fifth.suit]
+
+    def checkPair(self):
+        pair = []
+        others = []
+        for i in self.values:
+            if self.values.count(i) == 2:
+                pair.append(i)
+            elif self.values.count(i) == 1:
+                others.append(i)
+                others.sort()
+        score = 15 
+        return score
+
+    def checkTwoPair(self):
+        pairs = []
+        others = []
+        for i in self.values:
+            if self.values.count(i) == 2:
+                pairs.append(i)
+            elif self.values.count(i) == 1:
+                others.append(i)
+                others.sort()
+        score = 30
+
+    def scoreHand(self):
+        pass
+
+    def __str__(self):
+        return (f'{self.first}, {self.second}, {self.third}, {self.fourth}, {self.fifth}')
 
 class Deck:
     def __init__(self):
@@ -17,6 +50,7 @@ class Deck:
         self.createDeck()
         self.handsArr = []
         self.generateCombinations()
+        self.convertToHands()
 
     def createDeck(self):
         suits = ["spades", "clubes", "diamonds", "hearts"]
@@ -32,19 +66,22 @@ class Deck:
         self.handsArr = itertools.combinations(self.deck, 5)
         self.handsArr = list(self.handsArr)
 
+    def convertToHands(self):
+        index = 0
+        for i in self.handsArr:
+            self.handsArr[index] = Hand(i[0], i[1], i[2], i[3], i[4])
+            index += 1
 
     def printDeck(self):
         for i in self.deck:
             print(i)
 
-    #def printHands(self):
-    #    for i in self.handsArr:
-    #        print(i[0], i[1], i[2], i[3], i[4])
-
+    def printHands(self):
+        for i in self.handsArr:
+            print(i)
 
     #def shuffle(self):
 
 deck = Deck()
 deck.printDeck()
-#deck.printHands()
 
